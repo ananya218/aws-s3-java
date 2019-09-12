c76e6b8e51a582fa9245557f8f42a8121c9dc533
JoLo <54506108+jolo-dev@users.noreply.github.com>
2019-09-12 15:16:27 +0200

    Add via UI

---
+++ src/main/java/org/aws/s3/bucketfetcher/App.java
@@ -0,0 +1,13 @@
+package org.aws.s3.bucketfetcher;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
